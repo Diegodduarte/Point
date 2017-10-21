ab06a1959c271637633afab4ce0156dc791aa573
Diegodduarte <32546703+Diegodduarte@users.noreply.github.com>
2017-10-20 22:46:07 -0300

    Update point.py
    Adicionado o return

--- point.py
+++ point.py
@@ -6,6 +6,11 @@ class Point():
         self.x2 = x2
         self.y2 = y2
 
+    def SolicitarPontos(x,y,x2,y2):
+        x = float(input('Informe o primeiro Ponto x'))
+        y = float(input('Informe o primeiro Ponto y'))
+        x2 = float(input('Informe o segundo Ponto x'))
+        y2 = float(input('Informe o segundo Ponto y'))
 
     def __add__(self, other):
         P1x = self.x
@@ -15,13 +20,9 @@ class Point():
 
         SomaPontoX = P1x + P2x
         SomaPontoY = P1y + P2y
+        NovoPontox = SomaPontoX
+        NovoPontoY = SomaPontoY
 
-    def SolicitarPontos(self):
-        self.x = float(input('Informe o primeiro Ponto x'))
-        self.y = float(input('Informe o primeiro Ponto y'))
-        self.x2 = float(input('Informe o segundo Ponto x'))
-        self.y2 = float(input('Informe o segundo Ponto y'))
-
-
+        return (NovoPontox,NovoPontoY)
 
 
